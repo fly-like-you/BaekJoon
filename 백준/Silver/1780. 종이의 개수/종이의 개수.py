878c091d46a779f343556ba3d0eac23801841342
fly-like-you <58936850+fly-like-you@users.noreply.github.com>
2022-12-30 17:21:39 +0900

    [Silver II] Title: 종이의 개수, Time: 1532 ms, Memory: 169688 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -0,0 +1,28 @@
+N = int(input())
+array = []
+for i in range(N):
+    array.append(list(map(int, input().split())))
+ans = [0,0,0]
+
+
+def checksum(row, col, num):
+    start = array[row][col]
+    for i in range(row, row + num):
+        for j in range(col, col + num):
+            if start != array[i][j]:
+                return False
+    return True
+
+def divide(row, col, num):
+    if checksum(row, col, num):
+        ans[array[row][col]] += 1
+    else:
+        size = num // 3
+        for i in range(3):
+            for j in range(3):
+                divide(row + size * i, col + size * j, size)
+
+divide(0,0,N)
+print(ans[2])
+print(ans[0])
+print(ans[1])
