a46b701c5b59c6730a75200c21da5aafdde4ac57
fly-like-you <58936850+fly-like-you@users.noreply.github.com>
2023-01-27 20:14:16 +0900

    [Gold V] Title: 벼락치기, Time: 156 ms, Memory: 122388 KB -BaekjoonHub

---
+++ 백준/Gold/14728. 벼락치기/벼락치기.py
@@ -0,0 +1,19 @@
+N, K = map(int, input().split())
+
+W = [0]
+V = [0]
+for _ in range(N):
+    temp1, temp2 = map(int, input().split())
+    W.append(temp1)
+    V.append(temp2)
+
+dp = [ [ 0 for _ in range(K + 1)] for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    for j in range(1, K + 1):
+        if j >= W[i]:
+            dp[i][j] = max(dp[i-1][j-W[i]] + V[i], dp[i-1][j])
+        else:
+            dp[i][j] = dp[i-1][j]
+
+print(dp[N][K])
