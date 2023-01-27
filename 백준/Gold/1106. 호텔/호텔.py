291428631e49a5d1a26301ffad1f8664d2e8f3df
fly-like-you <58936850+fly-like-you@users.noreply.github.com>
2023-01-27 19:22:48 +0900

    [Gold V] Title: 호텔, Time: 120 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.py
+++ 백준/Gold/1106. 호텔/호텔.py
@@ -1,32 +1,17 @@
 C, N = map(int, input().split())
-promotion_cost = [0]
-promoted_client = [0]
+promotion_cost = []
+promoted_client = []
 for _ in range(N):
     cost, client = map(int, input().split())
     promotion_cost.append(cost)
     promoted_client.append(client)
-
+# C는 최소 인원
 inf = 100 * 1000
-dp = [ [ 0 for _ in range(inf + 1)] for _ in range(N + 1)]
+dp = [100 * C for _ in range(C + 100)]
+dp[0] = 0
+for i in range(N):
+    num_people = promoted_client[i]
+    for j in range(num_people, C + 100):
+        dp[j] = min(dp[j-num_people] + promotion_cost[i], dp[j])
+print(min(dp[C:]))
 
-
-for city_index in range(1,N+1):
-    for total_cost in range(1,inf + 1):
-
-        cost = promotion_cost[city_index]
-        client = promoted_client[city_index]
-
-        if total_cost - cost >= 0:
-            dp[city_index][total_cost] = max(dp[city_index - 1][total_cost], dp[city_index ][total_cost - cost] + client)
-
-        else:
-            dp[city_index][total_cost] = dp[city_index - 1][total_cost]
-
-        if dp[city_index][total_cost] >= C:
-            break
-
-for i in range(inf+1):
-    for j in range(N+1):
-        if dp[j][i] >= C:
-            print(i)
-            exit(0)
