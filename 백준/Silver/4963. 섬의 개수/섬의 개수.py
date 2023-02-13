4d2834b799e0a011829e8483fcd5c872e9a3907b
fly-like-you <58936850+fly-like-you@users.noreply.github.com>
2023-02-13 16:12:15 +0900

    [Silver II] Title: 섬의 개수, Time: 192 ms, Memory: 116980 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,5 +1,20 @@
 # my_code
 from collections import deque
+
+
+def bfs(pos_x, pos_y):
+    q = [(pos_x, pos_y)]
+    chart[pos_x][pos_y] = '0'
+    while q:
+        now_x, now_y = q.pop(0)
+        for i in range(8):
+            x = movable_pos_x[i] + now_x
+            y = movable_pos_y[i] + now_y
+            if x >= H or x < 0 or y >= W or y < 0:
+                continue
+            if chart[x][y] == '1':
+                chart[x][y] = '0'
+                q.append((x, y))
 while True:
     W, H = map(int, input().split())
     if W == 0 and H == 0:
@@ -8,23 +23,6 @@ while True:
 
     movable_pos_x = [1, 1, -1, -1, 1, -1, 0, 0]
     movable_pos_y = [1, -1, 1, -1, 0, 0, 1, -1]
-
-
-    def bfs(pos_x, pos_y):
-        queue = deque([[pos_x, pos_y]])
-        chart[pos_x][pos_y] = '0'
-        while queue:
-            now_pos = queue.popleft()
-            now_x, now_y = now_pos[0], now_pos[1]
-            for i in range(8):
-                x = movable_pos_x[i] + now_x
-                y = movable_pos_y[i] + now_y
-                if x >= H or x < 0 or y >= W or y < 0:
-                    continue
-                if chart[x][y] == '1':
-                    chart[x][y] = '0'
-                    queue.append([x, y])
-
 
     count = 0
     for i in range(H):
