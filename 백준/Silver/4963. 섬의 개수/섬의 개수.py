8a3bee1519bfe314df967b6f5aaab5bc0f4756ab
fly-like-you <58936850+fly-like-you@users.noreply.github.com>
2023-02-13 16:24:52 +0900

    [Silver II] Title: 섬의 개수, Time: 168 ms, Memory: 115932 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,38 +1,29 @@
-# my_code
-from collections import deque
+from sys import stdin
 
 
-def bfs(pos_x, pos_y):
-    q = [(pos_x, pos_y)]
-    chart[pos_x][pos_y] = '0'
-    while q:
-        now_x, now_y = q.pop(0)
-        for i in range(8):
-            x = movable_pos_x[i] + now_x
-            y = movable_pos_y[i] + now_y
-            if x >= H or x < 0 or y >= W or y < 0:
-                continue
-            if chart[x][y] == '1':
-                chart[x][y] = '0'
-                q.append((x, y))
+def bfs(x, y):
+    n = [(x, y)]
+    while n:
+        x, y = n.pop()
+        for deltaX, deltaY in delta:
+            X = x + deltaX
+            Y = y + deltaY
+            if 0 <= X < H and 0 <= Y < W and graph[X][Y] == '1':
+                graph[X][Y] = '0'
+                n.append((X, Y))
+
+
+read = stdin.readline
+delta = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, -1), (1, -1), (1, 1), (-1, 1)]
 while True:
-    W, H = map(int, input().split())
-    if W == 0 and H == 0:
+    W, H = map(int, read().split())
+    if W == H == 0:
         break
-    chart = [list(input().split()) for _ in range(H)]
-
-    movable_pos_x = [1, 1, -1, -1, 1, -1, 0, 0]
-    movable_pos_y = [1, -1, 1, -1, 0, 0, 1, -1]
-
     count = 0
+    graph = [list(read().split()) for _ in range(H)]
     for i in range(H):
         for j in range(W):
-            if chart[i][j] == '1':
+            if graph[i][j] == '1':
                 bfs(i, j)
                 count += 1
-
     print(count)
-
-
-
-#my_code와 #fast_code에서 #fast_code의 실행시간이 빠른 이유를 분석해줘
