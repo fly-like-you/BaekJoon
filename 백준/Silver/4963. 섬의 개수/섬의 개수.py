35f6780502cc8f17f2099aceca0933cb2a40c9ad
fly-like-you <58936850+fly-like-you@users.noreply.github.com>
2023-02-13 16:08:48 +0900

    [Silver II] Title: 섬의 개수, Time: 208 ms, Memory: 118196 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,15 +1,10 @@
+# my_code
 from collections import deque
-
-# W, H = 5, 4
-# chart = [[1, 0, 1, 0, 0],
-#          [1, 0, 0, 0, 0],
-#          [1, 0, 1, 0, 1],
-#          [1, 0, 0, 1, 0]]
 while True:
     W, H = map(int, input().split())
     if W == 0 and H == 0:
         break
-    chart = [list(map(int, input().split())) for _ in range(H)]
+    chart = [list(input().split()) for _ in range(H)]
 
     movable_pos_x = [1, 1, -1, -1, 1, -1, 0, 0]
     movable_pos_y = [1, -1, 1, -1, 0, 0, 1, -1]
@@ -17,7 +12,7 @@ while True:
 
     def bfs(pos_x, pos_y):
         queue = deque([[pos_x, pos_y]])
-        chart[pos_x][pos_y] = 0
+        chart[pos_x][pos_y] = '0'
         while queue:
             now_pos = queue.popleft()
             now_x, now_y = now_pos[0], now_pos[1]
@@ -26,16 +21,20 @@ while True:
                 y = movable_pos_y[i] + now_y
                 if x >= H or x < 0 or y >= W or y < 0:
                     continue
-                if chart[x][y] == 1:
-                    chart[x][y] = 0
+                if chart[x][y] == '1':
+                    chart[x][y] = '0'
                     queue.append([x, y])
 
 
     count = 0
     for i in range(H):
         for j in range(W):
-            if chart[i][j] == 1:
+            if chart[i][j] == '1':
                 bfs(i, j)
                 count += 1
 
     print(count)
+
+
+
+#my_code와 #fast_code에서 #fast_code의 실행시간이 빠른 이유를 분석해줘
