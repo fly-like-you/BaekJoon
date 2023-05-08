eccc5d08a601cbb538a0548aa01e147be75bf919
fly-like-you <58936850+fly-like-you@users.noreply.github.com>
2023-05-08 10:04:08 +0900

    [Bronze III] Title: 최댓값, Time: 244 ms, Memory: 18460 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int arr[][] = new int[10][10];
+        int max = 0;
+        int x = 0, y = 0;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = sc.nextInt();
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+
+        System.out.println(max);
+        System.out.println((x+1) + " " + (y+1));
+    }
+}
