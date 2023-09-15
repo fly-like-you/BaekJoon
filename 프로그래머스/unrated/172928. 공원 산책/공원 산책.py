4c1026d2a1e0317a6f7da44abab315764c0f1d26
fly-like-you <58936850+fly-like-you@users.noreply.github.com>
2023-09-15 23:01:36 +0900

    [unrated] Title: 공원 산책, Time: 0.08 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.py
@@ -1,68 +1,35 @@
-from copy import deepcopy
+def detect_dog(park):
+    for i in range(len(park)):
+        for j in range(len(park[i])):
+            if park[i][j] == "S":
+                return i, j
+
+class Dog:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.g = {"N": (-1, 0), "W": (0, -1), "E": (0, 1), "S": (1, 0)}
+
+    def move(self, park, direction, distance):
+        i, j = self.g[direction]
+        x, y = self.x + (i * distance), self.y + (j * distance)
+        if x < 0 or y < 0 or x >= len(park) or y >= len(park[0]):
+            return park
+        elif "X" in park[x][min(self.y, y): max(self.y, y + 1)] or \
+             "X" in [row[y] for row in park[min(self.x, x): max(self.x, x)]]:
+            return park
+
+        self.x = x
+        self.y = y
+        return park
 
 
 def solution(park, routes):
-    park_graph = []
+    park = [list(row) for row in park]
+    x, y = detect_dog(park)
+    dog = Dog(x, y)
 
-    for x in park:
-        park_graph.append(list(x))
-
-    col_len = len(list(zip(*park_graph))[0])
-
-    now = []
-
-    for i in range(len(park_graph)):
-        for j in range(len(park_graph[i])):
-            if park_graph[i][j] == "S":
-                now = [i, j]
-
-    for i in range(len(routes)):
-        x, y = now[0], now[1]
-        save = deepcopy(now)
-        direct, distance = routes[i].split()
-        distance = int(distance)
-
-
-        if direct == "E":  # 오른쪽 y
-            for i in range(distance):
-                if now[1] + 1 >= len(park_graph[0]):  # park_graph[x][y + distance]
-                    now = deepcopy(save)
-                    break
-                elif park_graph[now[0]][now[1] + 1] == "X":  # 장애물
-                    now = deepcopy(save)
-                    break
-                else:
-                    now[1] += 1
-
-        elif direct == "W":
-            for i in range(distance):
-                if now[1] - 1 < 0:
-                    now = deepcopy(save)
-                    break
-                elif park_graph[now[0]][now[1] - 1] == "X":  # 장애물
-                    now = deepcopy(save)
-                    break
-                else:
-                    now[1] -= 1
-        elif direct == "S":
-            for i in range(distance):
-                if now[0] + 1 >= col_len:  # park_graph[x][y + distance]
-                    now = deepcopy(save)
-                    break
-                elif park_graph[now[0] + 1][now[1]] == "X":  # 장애물
-                    now = deepcopy(save)
-                    break
-                else:
-                    now[0] += 1
-
-        elif direct == "N":
-            for i in range(distance):
-                if now[0] - 1 < 0:  # park_graph[x][y + distance]
-                    now = deepcopy(save)
-                    break
-                elif park_graph[now[0] - 1][now[1]] == "X":  # 장애물
-                    now = deepcopy(save)
-                    break
-                else:
-                    now[0] -= 1
-    return now
+    for route in routes:
+        direction, distance = route.split()
+        park = dog.move(park, direction, int(distance))
+    return [dog.x, dog.y]
