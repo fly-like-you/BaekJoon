e629d477652ce19d10e06628acbe1f9baf2d9995
Park Junho <58936850+fly-like-you@users.noreply.github.com>
2024-02-19 16:18:37 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 208 ms, Memory: 121864 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,51 +1,48 @@
-# 3 5
-# OOOPO
-# OIOOX
-# OOOXP
-# O는 빈 공간, X는 벽, I는 도연이, P는 사람이다.
-
+from sys import stdin
 from collections import deque
-
+read = stdin.readline
 N, M = map(int, input().split())
 
-campus_map = [list(input()) for _ in range(N)]
+# 목표는 내 실행 속도보다 빠른 코드 만들기!
+
+campus_map = []
+find_p = deque()
+visit = [[0] * M for _ in range(N)]
+
+for _ in range(N):
+    line = read().rstrip()
+    for __ in range(M):
+        if line[__] == 'I':
+            visit[_][__] = 1
+            find_p.append((_, __))
+    campus_map.append(line)
+
+
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
-def bfs(x, y):
-    answer = 0
-
-    queue = deque([[x,y]])
-    while queue:
-        coord = queue.popleft()
-        for i in range(4):
-            nx = coord[0] + dx[i]
-            ny = coord[1] + dy[i]
-            if not (0 <= nx < N and 0 <= ny < M):
-                continue
-
-            if campus_map[nx][ny] == 'P':
-                answer += 1
-                queue.append([nx, ny])
-                campus_map[nx][ny] = -1
-
-            elif campus_map[nx][ny] == 'O':
-                queue.append([nx, ny])
-                campus_map[nx][ny] = -1
-
-    return answer
-
 answer = 0
-for i in range(N):
-    for j in range(M):
-        if campus_map[i][j] == 'I':
-            campus_map[i][j] = -1
-            answer = bfs(i, j)
+
+while find_p:
+    coord = find_p.popleft()
+    for i in range(4):
+        nx = coord[0] + dx[i]
+        ny = coord[1] + dy[i]
+        if not (0 <= nx < N and 0 <= ny < M):
+            continue
+        if visit[nx][ny]:
+            continue
+
+        if campus_map[nx][ny] == 'P':
+            answer += 1
+            find_p.append([nx, ny])
+            visit[nx][ny] = -1
+
+        elif campus_map[nx][ny] == 'O':
+            find_p.append([nx, ny])
+            visit[nx][ny] = -1
+
+
+
 
 print(answer) if answer > 0 else print("TT")
-
-
-
-
-
-
