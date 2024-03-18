7efc98f0fd51f50ac9bfa3ca6ffae698b2785aed
Park Junho <58936850+fly-like-you@users.noreply.github.com>
2024-03-18 22:09:45 +0900

    [Gold V] Title: AC, Time: 108 ms, Memory: 38172 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -1,63 +1,33 @@
-# 4
-# RDD
-# 4
-# [1,2,3,4]
+import sys
+input = sys.stdin.readline
 
-#output
-# [2,1]
-# error
-def R(arr):
-    arr.reverse()
-
-def D(arr:list):
-    if len(arr) == 0:
-        return False
-
-    arr.pop(0)
-    return True
-
-
-def arr_p(arr):
-    return "[" + ",".join(map(str, arr)) + "]"
-
+def solution(CMD, length, arr):
+    r_state = 1
+    arr = arr.strip('[]\n').split(',')
+    a, b = 0, length
+    
+    for i in CMD:
+        if i == 'R':
+            r_state *= -1
+        elif i == 'D':
+            if r_state == 1:
+                a+=1
+            elif r_state == -1:
+                b-=1
+            if a > b:
+                return 'error'
+    
+    if r_state == 1:
+        return f'[{",".join(arr[a:b])}]'
+    elif r_state == -1:
+        if a == 0:
+            return f'[{",".join(arr[b-1::-1])}]'
+        else:
+            return f'[{",".join(arr[b-1:a-1:-1])}]'
 
 T = int(input())
-
-for _ in range(T):
-    CMDs = list(input())
+for i in range(T):
+    CMD = input()
     length = int(input())
-    if length:
-        arr = list(map(int, input()[1:-1].split(',')))
-    else:
-        _ = input()
-        arr = []
-
-    code = True
-    reverse_flag = False
-    for cmd in CMDs:
-        if cmd == 'R':
-            if reverse_flag:
-                reverse_flag = False
-            else:
-                reverse_flag = True
-
-        else:
-            if len(arr) == 0:
-                code = False
-                print('error')
-                break
-
-            if reverse_flag:
-                arr.pop(-1)
-            else:
-                arr.pop(0)
-
-    if code:
-        if reverse_flag:
-            R(arr)
-            print(arr_p(arr))
-        else:
-            print(arr_p(arr))
-
-
-
+    arr = input()
+    print(solution(CMD, length, arr))
