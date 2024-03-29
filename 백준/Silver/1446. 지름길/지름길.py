401d8e3f05f07ecfda073b9a420b3f94ba0a2d57
Park Junho <58936850+fly-like-you@users.noreply.github.com>
2024-03-29 15:50:04 +0900

    [Silver I] Title: 지름길, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -1,37 +1,23 @@
-N, d = map(int, input().split())
-shortcuts = []
-for i in range(N):
-    shortcut = list(map(int, input().split()))
-    if shortcut[1] <= d:
-        shortcuts.append(shortcut)
-
-shortcuts.sort()
-dp = {}
-dp[0] = 0
-dp[d] = d
-shortcuts_length = len(shortcuts)
-for i in range(shortcuts_length):
-    start = shortcuts[i][0]
-    end = shortcuts[i][1]
-    dp.setdefault(start, start)
-    dp.setdefault(end, end)
+n, d = map(int, input().split())
+shortcut = []
+for i in range(n): shortcut.append(list(map(int, input().split())))
+shortcut.sort()
+dp = [i for i in range(d + 1)]
 
 k = 0
-prev = 0
-for key in sorted(dp.keys()):
-    dp[key] = min(dp[key], dp[prev] + (key - prev))
-    while k < shortcuts_length:
-        start = shortcuts[k][0]
-        end = shortcuts[k][1]
-        dist = shortcuts[k][2]
-        if key == start:
+
+for i in range(d + 1):
+    dp[i] = min(dp[i-1] + 1, dp[i])
+
+    while k < n:
+        start = shortcut[k][0]
+        end = shortcut[k][1]
+        dist = shortcut[k][2]
+        if i == start:
             k += 1
-            dp[end] = min(dp[start] + dist, dp[end])
+            if end <= d:
+                dp[end] = min(dp[start] + dist, dp[end])
         else:
             break
-    prev = key
-
-
-
 
 print(dp[d])
