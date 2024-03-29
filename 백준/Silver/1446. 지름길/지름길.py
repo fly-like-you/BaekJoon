cf9a7129f3f51480f8ddc02dc1bdd94785f80b55
Park Junho <58936850+fly-like-you@users.noreply.github.com>
2024-03-29 16:47:48 +0900

    [Silver I] Title: 지름길, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -1,23 +1,15 @@
-n, d = map(int, input().split())
-shortcut = []
-for i in range(n): shortcut.append(list(map(int, input().split())))
-shortcut.sort()
-dp = [i for i in range(d + 1)]
-
-k = 0
-
-for i in range(d + 1):
-    dp[i] = min(dp[i-1] + 1, dp[i])
-
-    while k < n:
-        start = shortcut[k][0]
-        end = shortcut[k][1]
-        dist = shortcut[k][2]
-        if i == start:
-            k += 1
-            if end <= d:
-                dp[end] = min(dp[start] + dist, dp[end])
-        else:
-            break
-
+n,d = map(int,input().split())
+shortcut = [[] for _ in range(d+1)]
+for _ in range(n):
+    st,end,w = map(int,input().split())
+    if end <= d:
+        shortcut[end].append((st,w))
+dp = [0] * (d+1)
+dp[1] = 1
+for i in range(2,d+1):
+    if shortcut[i]:
+        cost = min([dp[st] + w for st,w in shortcut[i]])
+        dp[i] = min(dp[i-1] + 1, cost)
+    else:
+        dp[i] = dp[i-1] + 1
 print(dp[d])
