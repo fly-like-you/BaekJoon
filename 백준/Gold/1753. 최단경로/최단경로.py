1b758f637ef788ad4f05e63b878defc3902955e5
Park Junho <58936850+fly-like-you@users.noreply.github.com>
2024-04-30 14:52:07 +0900

    [Gold IV] Title: 최단경로, Time: 588 ms, Memory: 153548 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -0,0 +1,36 @@
+# 방향 그래프에서 시작점에서 다른 정점으로의 최단 경로 구하기
+
+import heapq
+import sys
+
+input = sys.stdin.readline
+V, E = map(int, input().split())
+K = int(input())
+graph = [[] for _ in range(V+1)]
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    graph[u].append([w, v])
+
+
+def dijkstra(V, start):
+    dist = [float('inf')] * (V + 1)
+    visited = [False] * (V + 1)
+    dist[start] = 0
+    heap = []
+    heapq.heappush(heap, [0, start])
+
+    while heap:
+        weight, node = heapq.heappop(heap)
+        visited[node] = True
+
+        for w, n in graph[node]:
+            cost = dist[node] + w
+            if not visited[n] and cost < dist[n]:
+                dist[n] = cost
+                heapq.heappush(heap, [cost, n])
+
+    return dist
+
+dist = dijkstra(V, K)
+for i in range(1, len(dist)):
+    print(dist[i] if dist[i] != float('inf') else 'INF')
