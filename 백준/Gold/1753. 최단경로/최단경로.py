498165df54bac0a303cf3af63d04ebfba355b9e6
Park Junho <58936850+fly-like-you@users.noreply.github.com>
2024-04-30 15:10:15 +0900

    [Gold IV] Title: 최단경로, Time: 552 ms, Memory: 135516 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -9,25 +9,26 @@ K = int(input())
 graph = [[] for _ in range(V+1)]
 for _ in range(E):
     u, v, w = map(int, input().split())
-    graph[u].append([w, v])
+    graph[u].append((w, v))
 
 
 def dijkstra(V, start):
     dist = [float('inf')] * (V + 1)
-    visited = [False] * (V + 1)
     dist[start] = 0
     heap = []
-    heapq.heappush(heap, [0, start])
+    heapq.heappush(heap, (0, start))
 
     while heap:
         weight, node = heapq.heappop(heap)
-        visited[node] = True
+
+        if weight > dist[node]:
+            continue
 
         for w, n in graph[node]:
             cost = dist[node] + w
-            if not visited[n] and cost < dist[n]:
+            if cost < dist[n]:
                 dist[n] = cost
-                heapq.heappush(heap, [cost, n])
+                heapq.heappush(heap, (cost, n))
 
     return dist
 
