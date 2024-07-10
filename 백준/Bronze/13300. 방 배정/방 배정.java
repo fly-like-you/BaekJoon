52b26463c83d3c3240c836a7c903f5571175e60a
Park Junho <58936850+fly-like-you@users.noreply.github.com>
2024-07-10 22:03:33 +0900

    [Bronze II] Title: 방 배정, Time: 96 ms, Memory: 14184 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[6][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int gender = Integer.parseInt(st.nextToken());
+            int grade = Integer.parseInt(st.nextToken());
+
+            arr[grade - 1][gender]++;
+        }
+        int roomCount = 0;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 2; j++) {
+                double room = Math.ceil((double) arr[i][j] / 2.0);
+                roomCount += room;
+            }
+        }
+        System.out.println(roomCount);
+    }
+}
