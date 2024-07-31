02e10e80aeb2a4aec488e5a6d5cd1dd334ff825d
Park Junho <58936850+fly-like-you@users.noreply.github.com>
2024-07-31 22:46:35 +0900

    [Gold IV] Title: 연구소 2, Time: 1716 ms, Memory: 301636 KB -BaekjoonHub

--- 백준/Gold/17141. 연구소 2/연구소 2.java
+++ 백준/Gold/17141. 연구소 2/연구소 2.java
@@ -1,59 +1,55 @@
+import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Deque;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int M;
-    static String[][] arr;
-    static int[][] times;
+    static int N; // 배열의 크기
+    static int R; // 놓을 수 있는 바이러스 수
+    static int C; // 총 바이러스 수
+    static int[][] arr;
+    static int[][] copy;
+    static int blank;
+    static int minTime;
+    static boolean flag;
 
-    static ArrayList<Integer> answer; // -> answer 하기
-    static ArrayList<Point> points;
-    static boolean[] visited;
+    static ArrayList<Point> virus;
+    static boolean[] selected;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
 
-        answer = new ArrayList<>();
-        arr = new String[N][N];
-        times = new int[N][N];
-        points = new ArrayList<>();
+        arr = new int[N][N];
+        copy = new int[N][N];
+        virus = new ArrayList<>();
+        minTime = Integer.MAX_VALUE;
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                arr[i][j] = st.nextToken();
-                if (arr[i][j].equals("2")) {
-                    points.add(new Point(i, j));
+                int elem = Integer.parseInt(st.nextToken());
+                arr[i][j] = copy[i][j] = elem;
+                if (elem == 0) {
+                    blank++;
+                }
+                if (elem == 2) {
+                    virus.add(new Point(i, j));
+                    C++;
                 }
             }
         }
-        visited = new boolean[points.size()];
-        combination(0, M);
-
-        int r = Integer.MAX_VALUE;
-        for (int i = 0; i < answer.size(); i++) {
-            r = Math.min(answer.get(i), r);
-        }
-        if (r == Integer.MAX_VALUE) {
-            System.out.println(-1);
-        } else {
-            System.out.println(r - 1);
-
-        }
-
+        blank += C - R;
+        selected = new boolean[C];
+        combination(0, R);
+        System.out.println(flag ? minTime-1 : -1);
     }
 
     static Queue<Point> q = new LinkedList<>();
@@ -61,6 +57,19 @@ public class Main {
     static int[] dy = {0, 0, 1, -1};
 
     private static void bfs() {
+        int cnt = 0;
+        copy();
+        for (int i = 0; i < C; i++) {
+            Point p = virus.get(i);
+            if (selected[i]) {
+                q.add(p);
+                arr[p.x][p.y] = 1;
+            } else {
+                arr[p.x][p.y] = 0;
+            }
+        }
+
+        int time = 1;
         while(!q.isEmpty()) {
             Point p = q.poll();
 
@@ -69,52 +78,32 @@ public class Main {
                 int ny = p.y + dy[i];
 
                 if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-                String lab = arr[nx][ny];
-                if (lab.equals("1")) continue;
-                if (times[nx][ny] == 0) {
-                    times[nx][ny] = times[p.x][p.y] + 1;
+                if (arr[nx][ny] == 0) {
+                    arr[nx][ny] = arr[p.x][p.y] + 1;
+                    time = Math.max(arr[nx][ny], time);
                     q.add(new Point(nx, ny));
+                    cnt++;
                 }
-
             }
         }
+        if (cnt == blank) {
+            minTime = Math.min(time, minTime);
+            flag = true;
+        }
+
     }
 
     private static void combination(int start, int end) {
         if (end == 0) {
-            times = new int[N][N];
-            for (int i = 0; i < visited.length; i++) {
-                if (visited[i]) {
-                    times[points.get(i).x][points.get(i).y] = 1;
-                    q.add(points.get(i));
-                }
-            }
-
-            // bfs 실행
             bfs();
-
-            // 해당 배열이 성공했나?
-            // 성공 했다면 시간은 얼마인가?
-            int rtv = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    rtv = Math.max(times[i][j], rtv);
-                    if (!arr[i][j].equals("1") && times[i][j] == 0) { // 실패
-                        return;
-                    }
-                }
-            }
-            answer.add(rtv);
-            return;
         }
 
-        for (int i = start; i < points.size(); i++) {
-            if (!visited[i]) {
-                visited[i] = true;
+        for (int i = start; i < virus.size(); i++) {
+            if (!selected[i]) {
+                selected[i] = true;
                 combination(start + 1, end - 1);
-                visited[i] = false;
+                selected[i] = false;
             }
-
         }
     }
 
@@ -122,6 +111,12 @@ public class Main {
         int x; int y;
         Point(int x, int y) {
             this.x = x; this.y = y;
+        }
+    }
+
+    static void copy() {
+        for (int i = 0; i < N; i++) {
+            System.arraycopy(copy[i], 0, arr[i], 0, N);
         }
     }
 
