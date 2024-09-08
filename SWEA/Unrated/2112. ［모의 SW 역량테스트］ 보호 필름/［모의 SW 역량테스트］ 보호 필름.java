7805bf098601f099c4995c32ca5c765d168c3e86
Park Junho <58936850+fly-like-you@users.noreply.github.com>
2024-09-08 13:39:08 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 379 ms, Memory: 86,768 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -4,89 +4,129 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Solution {
-    static int T, D, W, K;
-    static int minInject;
-    static char[][] film;
 
+public class Solution {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
+    static char[] A = {'0', '0', '0', '0', '0',  '0', '0', '0', '0', '0',
+            '0', '0', '0', '0', '0',  '0', '0', '0', '0', '0'};
+    static char[] B = {
+            '1', '1', '1', '1', '1',
+            '1', '1', '1', '1', '1',
+            '1', '1', '1', '1', '1',
+            '1', '1', '1', '1', '1',
+    };
+    static int T, D, W, K;
+    static int injectionCnt;
+    static int isSuccess;
+    static boolean[] injection;
+    static char[][] map;
+
     public static void main(String[] args) throws IOException {
         T = Integer.parseInt(br.readLine());
 
         for (int t = 1; t <= T; t++) {
-            sb.append("#").append(t).append(" ").append(solution()).append("\n");
+            st = new StringTokenizer(br.readLine());
+            D = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+
+            map = new char[D][W];
+            injection = new boolean[D];
+            injectionCnt = 0;
+
+            for (int i = 0; i < D; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < W; j++) {
+                    map[i][j] = st.nextToken().charAt(0);
+                }
+            }
+//            combination(0, 0, 1);
+
+            for (int d = 0; d < D; d++) {
+                isSuccess = 0;
+                combination(0, 0, d);
+                if (isSuccess > 0) {
+                    injectionCnt = d;
+                    break;
+                }
+            }
+
+            sb.append("#").append(t).append(" ").append(injectionCnt).append("\n");
         }
         System.out.println(sb);
     }
 
-    public static int solution() throws IOException {
-        st = new StringTokenizer(br.readLine());
-        D = Integer.parseInt(st.nextToken());
-        W = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        minInject = Integer.MAX_VALUE;
-        film = new char[D][W];
-
-        for (int i = 0; i < D; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < W; j++) {
-                film[i][j] = st.nextToken().charAt(0);
-            }
-        }
-
-        subset(0, 0);
-        return minInject;
-    }
-
-    public static void subset(int depth,  int injectCnt) {
-        if (injectCnt > minInject) {
-            return;
-        }
-        if (depth == D) {
-            if (filmTest()) {
-                minInject = Math.min(injectCnt, minInject);
+    private static void combination(int depth, int prev, int R) {
+        if (isSuccess > 0) return;
+        if (depth == R) {
+            // 약품 주입해보기
+            if (isSuccess()) {
+                isSuccess++;
             }
             return;
         }
 
-        subset(depth+1, injectCnt);
+        for (int i = prev; i < D; i++) {
+            // 약물을 주입할 배열을 true로 바꿔주기
+            char[] temp = map[i].clone();
+            map[i] = A;
+            combination(depth + 1, i + 1, R);
 
-        char[] copy = film[depth].clone();
-        Arrays.fill(film[depth], Character.forDigit(0, 10));
-        subset(depth+1, injectCnt+1);
+            map[i] = B;
+            combination(depth + 1, i + 1, R);
 
-        Arrays.fill(film[depth], Character.forDigit(1, 10));
-        subset(depth+1, injectCnt+1);
-
-        film[depth] = copy;
-
+            map[i] = temp;
+        }
     }
-    public static boolean filmTest() {
-        for (int i = 0; i < W; i++) {
-            if (!unitTest(i)) {
-                return false;
+
+
+
+    private static boolean isSuccess() {
+
+        A: for (int j = 0; j < W; j++) {
+            int length = 1;
+            char cur = map[0][j];
+            for (int i = 1; i < D; i++) {
+                // map[i][j] -> 행우선 탐색
+                if (map[i][j] == cur) {
+                    length++;
+                    if (length == K) {
+                        continue A;
+                    }
+                } else {
+                    length = 1;
+                }
+                cur = map[i][j];
             }
+            return false;
         }
         return true;
     }
-
-    public static boolean unitTest(int i) {
-        for (int j = 0; j <= D - K; j++) {
-            int flag = 0;
-
-            for (int k = j; k < j + K - 1; k++) {
-                if (film[k][i] == film[k+1][i]) {
-                    flag++;
-                }
-            }
-            if (flag == K - 1) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-
 }
+
+/*
+1. 약품 투입 위치 결정하기 (2개 가정, 순서 필요 없음, 중복 풀가능 -> 조합)
+2. 위치에 대해서 (A, B), (B, A)등 가능한 조합 시도하기 (순서 필요, 중복가능 -> 중복 순열 )
+3. 하던중에 테스트를 통과하면 종료
+ */
+/*
+1
+6 8 3
+0 0 1 0 1 0 0 1
+0 1 0 0 0 1 1 1
+0 1 1 1 0 0 0 0
+1 1 1 1 0 0 0 1
+0 1 1 0 1 0 0 1
+1 0 1 0 1 1 0 1
+
+1
+6 4 4
+1 1 0 0
+0 1 0 1
+1 1 0 1
+1 1 0 1
+1 1 0 1
+1 0 1 0
+ */
