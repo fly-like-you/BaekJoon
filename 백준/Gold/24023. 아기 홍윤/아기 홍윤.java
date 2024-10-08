6459b955d4f80f60fb94f976c3ee57f1ad816133
Park Junho <58936850+fly-like-you@users.noreply.github.com>
2024-10-08 23:26:02 +0900

    [Gold V] Title: 아기 홍윤, Time: 256 ms, Memory: 38940 KB -BaekjoonHub

--- 백준/Gold/24023. 아기 홍윤/아기 홍윤.java
+++ 백준/Gold/24023. 아기 홍윤/아기 홍윤.java
@@ -6,7 +6,6 @@ import java.util.StringTokenizer;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int[] arr;
     static int N, K;
 
     public static void main(String[] args) throws IOException {
@@ -15,29 +14,26 @@ public class Main {
         K = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
+        // 될 때까지 늘린다.
+        // 안되면 해당 인덱스 바로 다음부터 다시 시작
+        // 어쩌면 안되나? 현재 값과 K값의 & 결과가 0이면 끝
         int s = 0;
         int cur = 0;
         for (int i = 0; i < N; i++) {
-            cur |= arr[i];
+            int v =  Integer.parseInt(st.nextToken());
+            cur |= v;
             if (cur == K) {
                 System.out.println((s + 1) + " " + (i + 1));
                 return;
             }
-            if ((cur | K) != K || cur > K) {
+            if ((v | K) != K || cur > K) {
                 s = i + 1;
                 cur = 0;
             }
-
         }
+
         System.out.println(-1);
-        // 될 때까지 늘린다.
-        // 안되면 해당 인덱스 바로 다음부터 다시 시작
-        // 어쩌면 안되나? 현재 값과 K값의 & 결과가 0이면 끝
+
 
     }
 }
