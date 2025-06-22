6c9556117478077fd34e0eba359513b52f0607a8
Park Junho <58936850+fly-like-you@users.noreply.github.com>
2025-06-22 14:58:34 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 528 ms, Memory: 76396 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -9,45 +9,41 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         int N = Integer.parseInt(br.readLine());
-
         List<List<Integer>> list = new ArrayList<>();
+
         for (int i = 0; i <= N; i++) {
             list.add(new ArrayList<>());
         }
 
+        // 그래프 초기화
         for (int i = 0; i < N - 1; i++) {
             st = new StringTokenizer(br.readLine());
             int node1 = Integer.parseInt(st.nextToken());
             int node2 = Integer.parseInt(st.nextToken());
 
-            // 인접 리스트에 노드 추가
             list.get(node1).add(node2);
             list.get(node2).add(node1);
         }
 
-        // Tree 탐색하기
+        // BFS
         boolean[] visited = new boolean[N + 1];
-        Deque<Integer> q = new ArrayDeque<>();
-
-        // 정답 배열 채우기
-        // 시간 복잡도 = O(Nlog N)
         int[] answer = new int[N + 1];
-        q.add(1);
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.add(1);
         visited[1] = true;
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            List<Integer> neighbors = list.get(node);
-            for (Integer i : neighbors) {
+        while (!queue.isEmpty()) {
+            Integer node = queue.poll();
+            List<Integer> neighbor = list.get(node);
+            for (Integer i : neighbor) {
                 if (visited[i]) continue;
 
-                visited[i] = true;
                 answer[i] = node;
-                q.add(i);
+                queue.add(i);
+                visited[i] = true;
             }
         }
-
-        // 시간 복잡도 = O(N)
         StringBuilder sb = new StringBuilder();
+
         for (int i = 2; i <= N; i++) {
             sb.append(answer[i] + "\n");
         }
